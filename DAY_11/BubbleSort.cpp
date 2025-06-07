1508989250fce5cebca7b89e167723b23a6cb81f
Uday pathak <pathakuday45@gmail.com>
2025-06-07 06:46:36 +0530

    new question added

--- DAY_11/BubbleSort.cpp
+++ DAY_11/BubbleSort.cpp
@@ -11,7 +11,7 @@ void bubbleSort(int *arr, int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; i < n - j - 1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -26,3 +26,4 @@ int main()
     int arr[5] = {5, 4, 1, 3, 2};
     bubbleSort(arr, 5);
 }
+
