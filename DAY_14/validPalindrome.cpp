f4e69eef715545ef52cafc1bdcf1e19f8215a298
Uday pathak <pathakuday45@gmail.com>
2025-09-16 20:11:34 +0530

    isPalindrome

---
+++ DAY_14/validPalindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool isPalindrome(char arr[], int n)
+{
+    int start = 0,
+        end = n - 1;
+    while (start < end)
+    {
+        if (arr[start] != arr[end])
+        {
+            cout << "this is not a valid palindrome!";
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    cout << "this is a valid palindrome!";
+    return true;
+}
+int main()
+{
+    char arr[] = "aman";
+    isPalindrome(arr, strlen(arr));
+}
+
