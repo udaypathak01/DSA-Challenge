6228e5aae739b43c8576681161ea0f838ee7a5c4
Uday pathak <pathakuday45@gmail.com>
2025-09-20 10:18:32 +0530

    leetcode easy level problem solved named 231_power_of_two

--- leetcode/231_power_of_two.cpp
+++ leetcode/231_power_of_two.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int isPowerOfTwo(int n)
 {
-    
+    if (n < 1)
+    {
+        return 0;
+    }
+    while (n != 1)
+    {
+        if (n % 2 == 1) return 0;
+        else n /= 2;
+    }
+    return 1;
 }
 int main()
 {
